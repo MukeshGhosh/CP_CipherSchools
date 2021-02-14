b00ecd721a2898887965a2890e9cee0425805dc5
MukeshGhosh <47267510+MukeshGhosh@users.noreply.github.com>
2021-02-15 00:09:47 +0530

    Add files via upload

---
+++ Assignment 1 - Searching and Sorting/4)firstAndLastOccurences.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int findFirstPos(const vector<int>& v, int x){
+  int start = 0, end = v.size()-1, pos = -1;
+  while(start <= end){
+    int mid = start + (end - start)/2;
+    if(x < v[mid]){
+      end = mid - 1;
+    }
+    else if(x > v[mid]){
+      start = mid + 1;
+    }
+    else{
+      pos = mid;
+      end = mid - 1;
+    }
+  }
+  return pos;
+}
+
+int findLastPos(const vector<int>& v, int x){
+  int start = 0, end = v.size()-1, pos = -1;
+  while(start <= end){
+    int mid = start + (end - start)/2;
+    if(x < v[mid]){
+      end = mid - 1;
+    }
+    else if(x > v[mid]){
+      start = mid + 1;
+    }
+    else{
+      pos = mid;
+      start = mid + 1;
+    }
+  }
+  return pos;
+}
+
+int main(){
+  vector<int> v = {1, 3, 5, 5, 5, 5, 7, 123, 125};
+  int x = 5;
+  cout << "First Position: " << findFirstPos(v, x) << '\n';
+  cout << "Last Position: " << findLastPos(v, x) << '\n';
+  return 0;
+}
