24c28af382ae294b8619d1d62ee15b67553554f6
MukeshGhosh <47267510+MukeshGhosh@users.noreply.github.com>
2021-02-16 17:01:12 +0530

    Add files via upload

---
+++ Assignment 1 - Searching and Sorting/9)countingInversions.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int merge(vector<int> v, vector<int> temp, int start, int mid, int end){
+  int i = start, j = mid, k = start, count = 0;
+  while((i <= mid-1) && (j <= end)){
+    if(v[i] <= v[j]) temp[k++] = v[i++];
+    else{
+      temp[k++] = v[j++];
+      count += (mid-i);
+    }
+  }
+  while(i <= (mid-1)) temp[k++] = v[i++];
+  while(j <= end) temp[k++] = v[j++];
+  for(i = start; i <= end; i++){
+    v[i] = temp[i];
+  }
+  return count;
+}
+
+int mergeSort(vector<int> v, vector<int> temp, int start, int end){
+  int count = 0, mid;
+  if(start < end){
+    mid = (start + end)/2;
+    count += mergeSort(v, temp, start, mid);
+    count += mergeSort(v, temp, mid+1, end);
+    count += merge(v, temp, start, mid+1, end);
+  }
+  return count;
+}
+
+int main(){
+  vector<int> v = {1, 20, 6, 4, 5};
+  vector<int> temp(v.size());
+  int result = mergeSort(v, temp, 0, v.size()-1);
+  cout << result << '\n';
+  return 0;
+}
