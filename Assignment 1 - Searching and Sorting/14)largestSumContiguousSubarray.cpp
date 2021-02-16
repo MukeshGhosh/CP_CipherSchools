de45ecdc938fd22d0f93cc3241c33103a4153821
MukeshGhosh <47267510+MukeshGhosh@users.noreply.github.com>
2021-02-16 15:41:57 +0530

    Add files via upload

---
+++ Assignment 1 - Searching and Sorting/14)largestSumContiguousSubarray.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int findSum(const vector<int>& v){
+  int maxSum = v[0], currSum = v[0];
+  for(int i = 1; i < v.size(); i++){
+    currSum = max(currSum + v[i], v[i]);
+    maxSum = max(maxSum, currSum);
+  }
+  return maxSum;
+}
+
+int main(){
+
+  vector<int> v = {-2, -3, 4, -1, -2, 1, 5, -3};
+  cout << findSum(v) << '\n';
+  return 0;
+}
